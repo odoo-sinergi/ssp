c29a06a2f8eb6493a5d332325f0077d647a1d2c7
fidiarya <fidiarya@gmail.com>
2023-03-01 08:20:27 +0700

    update

--- od_journal_sequence/models/account_move.py
+++ od_journal_sequence/models/account_move.py
@@ -28,6 +28,7 @@ class AccountMove(models.Model):
             return
         return journal.refund_sequence_id
 
+
     def _post(self, soft=True):
         """Post/Validate the documents.
 
@@ -42,59 +43,82 @@ class AccountMove(models.Model):
             Nothing will be performed on those documents before the accounting date.
         :return Model<account.move>: the documents that have been posted
         """
+        if not self.env.su and not self.env.user.has_group('account.group_account_invoice'):
+            raise AccessError(_("You don't have the access rights to post an invoice."))
+
+        for invoice in self.filtered(lambda move: move.is_invoice(include_receipts=True)):
+            if invoice.quick_edit_mode and invoice.quick_edit_total_amount and invoice.quick_edit_total_amount != invoice.amount_total:
+                raise UserError(_(
+                    "The current total is %s but the expected total is %s. In order to post the invoice/bill, "
+                    "you can adjust its lines or the expected Total (tax inc.).",
+                    formatLang(self.env, invoice.amount_total, currency_obj=invoice.currency_id),
+                    formatLang(self.env, invoice.quick_edit_total_amount, currency_obj=invoice.currency_id),
+                ))
+            if invoice.partner_bank_id and not invoice.partner_bank_id.active:
+                raise UserError(_(
+                    "The recipient bank account linked to this invoice is archived.\n"
+                    "So you cannot confirm the invoice."
+                ))
+            if float_compare(invoice.amount_total, 0.0, precision_rounding=invoice.currency_id.rounding) < 0:
+                raise UserError(_(
+                    "You cannot validate an invoice with a negative total amount. "
+                    "You should create a credit note instead. "
+                    "Use the action menu to transform it into a credit note or refund."
+                ))
+
+            if not invoice.partner_id:
+                if invoice.is_sale_document():
+                    raise UserError(_("The field 'Customer' is required, please complete it to validate the Customer Invoice."))
+                elif invoice.is_purchase_document():
+                    raise UserError(_("The field 'Vendor' is required, please complete it to validate the Vendor Bill."))
+
+            # Handle case when the invoice_date is not set. In that case, the invoice_date is set at today and then,
+            # lines are recomputed accordingly.
+            if not invoice.invoice_date:
+                if invoice.is_sale_document(include_receipts=True):
+                    invoice.invoice_date = fields.Date.context_today(self)
+                elif invoice.is_purchase_document(include_receipts=True):
+                    raise UserError(_("The Bill/Refund date is required to validate this document."))
+
         if soft:
             future_moves = self.filtered(lambda move: move.date > fields.Date.context_today(self))
             for move in future_moves:
                 if move.auto_post == 'no':
                     move.auto_post = 'at_date'
-            for move in future_moves:
-                msg = _('This move will be posted at the accounting date: %(date)s',
-                        date=format_date(self.env, move.date))
+                msg = _('This move will be posted at the accounting date: %(date)s', date=format_date(self.env, move.date))
                 move.message_post(body=msg)
             to_post = self - future_moves
         else:
             to_post = self
 
-        # `user_has_group` won't be bypassed by `sudo()` since it doesn't change the user anymore.
-        if not self.env.su and not self.env.user.has_group('account.group_account_invoice'):
-            raise AccessError(_("You don't have the access rights to post an invoice."))
         for move in to_post:
-            if move.line_ids.filtered(lambda line: not line.display_type):
+            if move.state == 'posted':
+                raise UserError(_('The entry %s (id %s) is already posted.') % (move.name, move.id))
+            if not move.line_ids.filtered(lambda line: line.display_type not in ('line_section', 'line_note')):
                 raise UserError(_('You need to add a line before posting.'))
-            if move.auto_post and move.date > fields.Date.context_today(self):
+            if move.auto_post != 'no' and move.date > fields.Date.context_today(self):
                 date_msg = move.date.strftime(get_lang(self.env).date_format)
                 raise UserError(_("This move is configured to be auto-posted on %s", date_msg))
-
-            if not move.partner_id:
-                if move.is_sale_document():
-                    raise UserError(
-                        _("The field 'Customer' is required, please complete it to validate the Customer Invoice."))
-                elif move.is_purchase_document():
-                    raise UserError(
-                        _("The field 'Vendor' is required, please complete it to validate the Vendor Bill."))
-
-            if move.is_invoice(include_receipts=True) and float_compare(move.amount_total, 0.0,
-                                                                        precision_rounding=move.currency_id.rounding) < 0:
+            if not move.journal_id.active:
                 raise UserError(_(
-                    "You cannot validate an invoice with a negative total amount. You should create a credit note instead. Use the action menu to transform it into a credit note or refund."))
+                    "You cannot post an entry in an archived journal (%(journal)s)",
+                    journal=move.journal_id.display_name,
+                ))
+            if move.display_inactive_currency_warning:
+                raise UserError(_(
+                    "You cannot validate a document with an inactive currency: %s",
+                    move.currency_id.name
+                ))
 
-            # Handle case when the invoice_date is not set. In that case, the invoice_date is set at today and then,
-            # lines are recomputed accordingly.
-            # /!\ 'check_move_validity' must be there since the dynamic lines will be recomputed outside the 'onchange'
-            # environment.
-            if not move.invoice_date and move.is_invoice(include_receipts=True):
-                move.invoice_date = fields.Date.context_today(self)
-                move.with_context(check_move_validity=False)._onchange_invoice_date()
+            if move.line_ids.account_id.filtered(lambda account: account.deprecated):
+                raise UserError(_("A line of this move is using a deprecated account, you cannot post it."))
 
-            # When the accounting date is prior to the tax lock date, move it automatically to the next available date.
-            # /!\ 'check_move_validity' must be there since the dynamic lines will be recomputed outside the 'onchange'
-            # environment.
-            if (move.company_id.tax_lock_date and move.date <= move.company_id.tax_lock_date) and (
-                    move.line_ids.tax_ids or move.line_ids.tax_tag_ids):
-                move.date = move.company_id.tax_lock_date + timedelta(days=1)
-                move.with_context(check_move_validity=False)._onchange_currency()
-            
-            date = self.stock_move_id.picking_id.force_date
+            affects_tax_report = move._affect_tax_report()
+            lock_dates = move._get_violated_lock_dates(move.date, affects_tax_report)
+            if lock_dates:
+                move.date = move._get_accounting_date(move.invoice_date or move.date, affects_tax_report)
+
+            date = move.stock_move_id.picking_id.force_date
             if date:
                 if date != move.date:
                     move.date = date
@@ -111,57 +135,69 @@ class AccountMove(models.Model):
                 move.name = sequence.with_context(ir_sequence_date=move.date).next_by_id()
 
         # Create the analytic lines in batch is faster as it leads to less cache invalidation.
-        to_post.mapped('line_ids')._create_analytic_lines()
+        to_post.line_ids._create_analytic_lines()
+
+        # Trigger copying for recurring invoices
+        to_post.filtered(lambda m: m.auto_post not in ('no', 'at_date'))._copy_recurring_entries()
+
+        for invoice in to_post:
+            # Fix inconsistencies that may occure if the OCR has been editing the invoice at the same time of a user. We force the
+            # partner on the lines to be the same as the one on the move, because that's the only one the user can see/edit.
+            wrong_lines = invoice.is_invoice() and invoice.line_ids.filtered(lambda aml:
+                aml.partner_id != invoice.commercial_partner_id
+                and aml.display_type not in ('line_note', 'line_section')
+            )
+            if wrong_lines:
+                wrong_lines.write({'partner_id': invoice.commercial_partner_id.id})
+
         to_post.write({
             'state': 'posted',
             'posted_before': True,
         })
 
-        for move in to_post:
-            move.message_subscribe([p.id for p in [move.partner_id] if p not in move.sudo().message_partner_ids])
+        for invoice in to_post:
+            invoice.message_subscribe([
+                p.id
+                for p in [invoice.partner_id]
+                if p not in invoice.sudo().message_partner_ids
+            ])
 
             # Compute 'ref' for 'out_invoice'.
-            if move._get_invoice_reference_odoo_partner():
+            if invoice.move_type == 'out_invoice' and not invoice.payment_reference:
                 to_write = {
-                    'payment_reference': move._get_invoice_computed_reference(),
+                    'payment_reference': invoice._get_invoice_computed_reference(),
                     'line_ids': []
                 }
-                for line in move.line_ids.filtered(
-                        lambda line: line.account_id.account_type in ('asset_receivable', 'liability_payable')):
+                for line in invoice.line_ids.filtered(lambda line: line.account_id.account_type in ('asset_receivable', 'liability_payable')):
                     to_write['line_ids'].append((1, line.id, {'name': to_write['payment_reference']}))
-                move.write(to_write)
+                invoice.write(to_write)
 
-        for move in to_post:
-            if move.is_sale_document() \
-                    and move.journal_id.sale_activity_type_id \
-                    and (move.journal_id.sale_activity_user_id or move.invoice_user_id).id not in (
-            self.env.ref('base.user_root').id, False):
-                move.activity_schedule(
-                    date_deadline=min((date for date in move.line_ids.mapped('date_maturity') if date),
-                                      default=move.date),
-                    activity_type_id=move.journal_id.sale_activity_type_id.id,
-                    summary=move.journal_id.sale_activity_note,
-                    user_id=move.journal_id.sale_activity_user_id.id or move.invoice_user_id.id,
+            if (
+                invoice.is_sale_document()
+                and invoice.journal_id.sale_activity_type_id
+                and (invoice.journal_id.sale_activity_user_id or invoice.invoice_user_id).id not in (self.env.ref('base.user_root').id, False)
+            ):
+                invoice.activity_schedule(
+                    date_deadline=min((date for date in invoice.line_ids.mapped('date_maturity') if date), default=invoice.date),
+                    activity_type_id=invoice.journal_id.sale_activity_type_id.id,
+                    summary=invoice.journal_id.sale_activity_note,
+                    user_id=invoice.journal_id.sale_activity_user_id.id or invoice.invoice_user_id.id,
                 )
 
         customer_count, supplier_count = defaultdict(int), defaultdict(int)
-        for move in to_post:
-            if move.is_sale_document():
-                customer_count[move.partner_id] += 1
-            elif move.is_purchase_document():
-                supplier_count[move.partner_id] += 1
+        for invoice in to_post:
+            if invoice.is_sale_document():
+                customer_count[invoice.partner_id] += 1
+            elif invoice.is_purchase_document():
+                supplier_count[invoice.partner_id] += 1
         for partner, count in customer_count.items():
             (partner | partner.commercial_partner_id)._increase_rank('customer_rank', count)
         for partner, count in supplier_count.items():
             (partner | partner.commercial_partner_id)._increase_rank('supplier_rank', count)
 
-        # Trigger action for paid invoices in amount is zero
+        # Trigger action for paid invoices if amount is zero
         to_post.filtered(
             lambda m: m.is_invoice(include_receipts=True) and m.currency_id.is_zero(m.amount_total)
         )._invoice_paid_hook()
 
-        # Force balance check since nothing prevents another module to create an incorrect entry.
-        # This is performed at the very end to avoid flushing fields before the whole processing.
-        container = {'records': self, 'self': self}
-        to_post._check_balanced(container)
         return to_post
