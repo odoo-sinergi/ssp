d46d2a86efa7309c226a21c0662fc1a6d5b0ee10
aditya_demas <adityademas@gmail.com>
2023-03-28 09:13:48 +0700

    up

--- form_standard_odoo/__manifest__.py
+++ form_standard_odoo/__manifest__.py
@@ -27,22 +27,14 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-
-        # 'report/sdt_standard_good_receipt.xml',
-        # 'report/sdt_standard_inventory_transfer.xml',
-        # 'report/sdt_standard_payment_receipt.xml',
-        # 'report/sdt_standard_payment.xml',
         'wizard/sdt_payment.xml',
         'views/account_payment.xml',
+        # report
         'report/sdt_standard_purchase_order.xml',
-        # 'report/sdt_standard_purchase_request_quotation.xml',
         'report/sdt_standard_sales_invoice.xml',
         'report/sdt_standard_form_voucher.xml',
         'report/sdt_standard_delivery_order.xml',
         'report/sdt_standard_cash_advance.xml',
-        # 'report/sdt_standard_sales_quotation.xml',
-        # 'report/sdt_standard_vendor_bill.xml',
     ],
     # only loaded in demonstration mode
 }
