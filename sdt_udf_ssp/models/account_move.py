7ce042b51961fdbce84e9d2d771cb77dfb33d41d
aditya_demas <adityademas@gmail.com>
2023-03-31 16:07:00 +0700

    up

--- sdt_udf_ssp/models/account_move.py
+++ sdt_udf_ssp/models/account_move.py
@@ -10,18 +10,18 @@ class AccountMove(models.Model):
 
     picking_tt_ids = fields.Many2many(string='Stock Picking DO',comodel_name='stock.picking', relation='account_move_stock_picking_tt',column1='move_id',column2='Picking_tt_id',)
     # fields fix
-    stock_picking_tt_ids = fields.Many2many(string='Picking DO',comodel_name='stock.picking', relation='account_picking_tt_rel',column1='move_id',column2='picking_id',)
+    stock_picking_tt_ids = fields.Many2many(string='Delivery Order',comodel_name='stock.picking', relation='account_picking_tt_rel',column1='move_id',column2='picking_id',)
     picking_tt = fields.Boolean(string='Picking DO',)
     
 
-    @api.onchange('stock_picking_tt_ids','picking_tt')
-    def onchange_stock_picking_tt_ids(self):
-        picking_obj = self.env['stock.picking'].search([('origin', '=', self.invoice_origin),('picking_type_code', '=', 'internal')])
-        picking_list=[]
-        for line in picking_obj:
-            picking_list.append(line.id)
-        domain = {'stock_picking_tt_ids': [('id', '=', picking_list)]}
-        return {'domain': domain} 
+    # @api.onchange('stock_picking_tt_ids','picking_tt')
+    # def onchange_stock_picking_tt_ids(self):
+    #     picking_obj = self.env['stock.picking'].search([('origin', '=', self.invoice_origin),('picking_type_code', '=', 'internal')])
+    #     picking_list=[]
+    #     for line in picking_obj:
+    #         picking_list.append(line.id)
+    #     domain = {'stock_picking_tt_ids': [('id', '=', picking_list)]}
+    #     return {'domain': domain} 
     
     def action_post(self):
         res = super().action_post()
