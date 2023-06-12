13655401880ec4d74e984a81b9b6e9ffd446cb2c
imamhusyaeni <imam9835@gmail.com>
2023-06-12 09:48:51 +0700

    up part no

--- sdt_udf_ssp/models/product.py
+++ sdt_udf_ssp/models/product.py
@@ -7,6 +7,7 @@ class ProductTemplate(models.Model):
     link_reference = fields.Char(string='Link Reference')
     spesifikasi = fields.Char(string='Spesifikasi')
     merk = fields.Char(string='Merk')
+    part_no = fields.Char(string='Part No.')
 
 
 class ProductProduct(models.Model):
